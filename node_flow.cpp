878850d2f067c16a8fc94d63a4f17ad98fda6acf
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-07 15:11:52 +0100

    Basic lib node flow

--- node_flow.cpp
+++ node_flow.cpp
@@ -1,6 +1,58 @@
-#include "node_flow.h"
+/**
+  * @file    NodeFlow.cpp
+  * @version 1.0.0
+  * @author  
+  * @brief   Header file of the SX1276 driver module. 
+  * A library which implements a software flow for thingpilot Node Core powered modules. 
+  * Controls the device configuration, OTA config updates, security, sensor read timing, 
+  * telemetry transmit timing and provides pure virtual functions for the application developer 
+  * to implement application device specific logic
+  */
 
-// Constructor
-NodeFlow::NodeFlow() {
-    
+
+/** Includes 
+ */
+#include "node_flow.h"
+TPL5010 tpl5010(PinName done);
+
+NodeFlow::NodeFlow(){
+
 }
+// 
+
+/* Kick wdg, read flags*/
+void NodeFlow::Init()
+{
+   init_eeprom();
+   
+   
+   tpflags a={1}; 
+   a.flag; //set state in debug return value from eeprom, default 0
+}
+
+void NodeFlow::NodeFlowStart()
+{
+   
+   tpflags a={1}; 
+}
+
+void NodeFlow::PeriodicSenseHandler()
+{
+    tpflags a={2};
+}
+
+void NodeFlow::TriggerSenseHandler()
+{
+    tpflags a={3};
+}
+
+void NodeFlow::init_eeprom()
+{
+    //
+}
+
+
+
+
+
+
--- node_flow.h
+++ node_flow.h
@@ -1,13 +1,73 @@
 
-#include "STM24256.h"
+/**
+  * @file    NodeFlow.h
+  * @version 1.0.0
+  * @author  
+  * @brief   Header file of the SX1276 driver module. 
+  * A library which implements a software flow for thingpilot Node Core powered modules. 
+  * Controls the device configuration, OTA config updates, security, sensor read timing, 
+  * telemetry transmit timing and provides pure virtual functions for the application developer 
+  * to implement application device specific logic
+  */
+
+/** Includes 
+ */
+
+#ifndef MBED_NODEFLOW_H
+#define MBED_NODEFLOW_H
+
+
 #include "TPL5010.h"
-//#include "thingpilot_telemetry_driver.h"
-//#include "SaraNx.h"
+#include "tformatter.h"
+#include "DataManager.h"
+#include "SaraN2.h"
+#include "LorawanTP.h"
 
 
-class NodeFlow {
-    public:
-        NodeFlow();
-    private:
+        
+struct tpflags {
+
+        //3-bit flag, diff compination deferent flag code
+        unsigned int flag : 3; 
+        // 0.in_reset_mode=   000
+        // 1.in_debug_mode=   001
+        // 2.in_read_mode=    010
+        // 3.in_trigger_mode= 011
+        // 4.in_sleep_mode=   100
 
 };
+
+
+/** Base class for the Nodeflow
+ */      
+class NodeFlow {
+    public:
+    /*Constructor*/
+    NodeFlow();  //Node flow as an InterruptHandler
+    
+    /*Deconstructor*/
+    ~NodeFlow();
+    
+    /*Interrupts*/
+    void NodeFlowStart(); //kick wdg, read flags
+    void Init(); //initialise device, init_eeprom, set flag to debug
+    void PeriodicSenseHandler(); //
+    void TriggerSenseHandler();
+    
+    private:
+    /* Actions */
+    void init_eeprom(); 
+    void get_periodic_sensors(); //periodic
+    void get_trigger_sensors(); //Sensors driver
+    void enter_standby(); //watchdog driver
+
+    float get_supply_voltage();
+
+    TPL5010 tpl5010(PinName done);
+    
+    
+
+};
+
+
+#endif
