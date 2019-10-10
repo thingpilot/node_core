84d660f699683b57684848c6811b684206e6eb90
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-10 17:46:39 +0100

    add sensor config to the eeprom, sensor id, sensor type, reading time

--- node_flow.cpp
+++ node_flow.cpp
@@ -4,6 +4,7 @@
 int status = -1;
 Serial pc(PC_TXD, PC_RXD);
 
+
 NodeFlow::NodeFlow(PinName write_control, PinName sda, PinName scl, int frequency_hz): 
  DataManager(write_control, sda, scl, frequency_hz) {
 
@@ -12,10 +13,13 @@ NodeFlow::NodeFlow(PinName write_control, PinName sda, PinName scl, int frequenc
 
  NodeFlow::~NodeFlow() {
 
-
  }
 
 
+
+/** Eeprom configuration- how the eeprom will look like which files initialised
+ *  
+ */
     union DeviceConfig
 {
     struct 
@@ -31,11 +35,25 @@ NodeFlow::NodeFlow(PinName write_control, PinName sda, PinName scl, int frequenc
     char data[sizeof(DeviceConfig::parameters)];
 };
 
+    union SensorConfig
+{
+    struct 
+    {
+        uint16_t device_id;
+        uint16_t device_type;
+        uint16_t time_comparator; 
+    
+    } parameters;
+
+    char data[sizeof(SensorConfig::parameters)];
+};
+
 union SensorA
 {
     struct 
     {
-        float temp;
+        float value;
+        uint16_t device_type;
         int timestamp;
     } parameters;
 
@@ -46,19 +64,39 @@ union SensorB
 {
     struct 
     {
-        uint16_t hum;
+        uint16_t value;
+        uint16_t device_type;
         int timestamp;
     } parameters;
 
     char data[sizeof(SensorB::parameters)];
 };
+union BatteryVoltage
+{
+    struct 
+    {
+        uint16_t device_type;
+        int timestamp;
+
+
+    } parameters;
+
+    char data[sizeof(BatteryVoltage::parameters)];
+};
 
 enum Filenames
 {
-    DeviceConfig_n = 0,
-    SensorA_n      = 1,
-    SensorB_n      = 2
+    DeviceConfig_n   = 0,
+    SensorA_n        = 1,
+    SensorB_n        = 2,
+    SensorConfig_n   = 3,
+    BatteryVoltage_n = 9   
 };
+
+int NodeFlow::start(){
+
+    initialise();
+}
 
 int NodeFlow::initialise(){
 
@@ -67,21 +105,102 @@ int NodeFlow::initialise(){
 
  bool initialised = false;
  if(DataManager::is_initialised(initialised)!=true){
-   
+ pc.printf("Filesystem initialisation failed");   
     return 255;
  }
- //pc.printf("is_initialised status: %i, is initialised: %i\r\n", status, initialised);
+ //
    else {
+       pc.printf("init_filesystem status: %i\r\n", status);
        return status;
    }
 }
 
 int NodeFlow::get_global_stats() {
     DataManager_FileSystem::GlobalStats_t g_stats;
-
-
     status = DataManager::get_global_stats(g_stats.data);
     DataManager::print_global_stats(pc, g_stats);
  
  return status;
 }
+
+int NodeFlow::add_data_config_file(uint16_t entries_to_store,uint16_t device_id,int timestamp,
+                            uint16_t mode, uint16_t property, uint8_t flag,uint8_t cool){
+
+    DataManager_FileSystem::File_t DeviceConfig_File_t;
+    DeviceConfig_File_t.parameters.filename = DeviceConfig_n;
+    DeviceConfig_File_t.parameters.length_bytes = sizeof(DeviceConfig::parameters);
+
+     if(DataManager::add_file(DeviceConfig_File_t, entries_to_store)!=0){
+        
+         pc.printf("Unsuccess! status: %i\r\n", status);
+     }
+
+     else{
+        pc.printf("add_file status: %i\r\n", status);
+        
+        DeviceConfig w_conf;
+        w_conf.parameters.device_id = device_id;
+        w_conf.parameters.timestamp = timestamp;
+        w_conf.parameters.flag = mode;
+        w_conf.parameters.mode = property;
+        w_conf.parameters.property = flag;
+        w_conf.parameters.cool = cool;
+        for(int i = 0; i < 2; i++)
+    {
+        w_conf.parameters.device_id = i;
+        status = DataManager::append_file_entry(DeviceConfig_n, w_conf.data, sizeof(w_conf.parameters));
+        pc.printf("append_file_entry attempt %i status: %i\r\n", i, status);
+    }
+     }
+    
+return status;
+}
+/*Device config, Sensor_1-8*/
+ int NodeFlow::get_file_parameters(uint8_t filename, DataManager_FileSystem::File_t &file){
+    
+    status = DataManager::get_file_by_name(filename, file);
+    DataManager::print_file(pc, file);
+ }
+
+ int NodeFlow::add_sensors( uint16_t device_id[],uint16_t device_type[],uint16_t reading_time[],
+                             uint16_t number_of_sensors) {
+  
+    if (number_of_sensors>8){
+        pc.printf("Error more than 8 sensors %i\r\n", status);   
+        return -1; 
+    }
+
+    else {
+    for (int i=0; i<=number_of_sensors; i++){
+    DataManager_FileSystem::File_t SensorConfig_File_t;
+    SensorConfig_File_t.parameters.filename = SensorConfig_n;
+    SensorConfig_File_t.parameters.length_bytes = sizeof(SensorConfig::parameters);
+
+     if(DataManager::add_file(SensorConfig_File_t, 1)!=0){
+        pc.printf("Unsuccess! status: %i\r\n", status);
+     }
+
+     else{
+        pc.printf("add_file status: %i\r\n", status);
+        
+        DeviceConfig s_conf;
+        s_conf.parameters.device_id = device_id[i];
+        s_conf.parameters.device_type = device_type[1];
+         s_conf.parameters.time_comparator=reading_time[i];
+       
+        for(int i = 0; i < 2; i++){
+        s_conf.parameters.device_id = i;
+        status = DataManager::append_file_entry(DeviceConfig_n, s_conf.data, sizeof(s_conf.parameters));
+        pc.printf("append_file_entry attempt %i status: %i\r\n", i, status);
+        }    
+     }
+    }
+     return status;
+    }
+}
+
+
+
+
+
+
--- node_flow.h
+++ node_flow.h
@@ -9,11 +9,39 @@ class NodeFlow: public DataManager{
 
     ~NodeFlow();
 
+
+    /** Check to see if filesystem is initialised
+     */
+    int start();
+
     int initialise(); 
 
+    /** Read global stats
+     */
     int get_global_stats();
 
+    /** Let's see all of our newly created file's parameters
+     */
+    int get_file_parameters(uint8_t filename, DataManager_FileSystem::File_t &file);
+
+    int add_sensors(uint16_t device_id[],uint16_t device_type[],uint16_t reading_time[],
+                    uint16_t number_of_sensors);
 
 
+
+    private:
+
+    /** Add a new file to EEPROM that only accepts a single entry.
+     *  When adding a new file, despite the File_t type having many parameters,
+     *   we only need to define the filename and length_bytes as shown
+     */
+    int add_data_config_file(uint16_t entries_to_store, uint16_t device_id,int timestamp,
+                            uint16_t mode, uint16_t property, uint8_t flag,uint8_t cool);
+
+
+    
+
+    
+    
     
 };
