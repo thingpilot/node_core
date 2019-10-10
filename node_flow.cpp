4fee29219fffbc337e8e45b889110b88b8d836c3
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-10 14:17:17 +0100

    Deleted unnecessary components

--- node_flow.cpp
+++ node_flow.cpp
@@ -1,26 +1,6 @@
-/**
-  * @file    NodeFlow.cpp
-  * @version 1.0.0
-  * @author  
-  * @brief   C++ file of the NodeFlow.cpp. 
-  * A library which implements a software flow for thingpilot Node Core powered modules. 
-  * Controls the device configuration, OTA config updates, security, sensor read timing, 
-  * telemetry transmit timing and provides pure virtual functions for the application developer 
-  * to implement application device specific logic
-  */
-
-
-/** Includes 
- */
 #include "node_flow.h"
-Serial pc(PC_TXD,PC_RXD);
-/**
- *  Initialise device, sets default configuration where needed,
- *  Initialise rtc and get_wakeup_type
- *  Must be called by the user ex. mydevice.Initialise();
- */
 
- NodeFlow::NodeFlow(PinName write_control, PinName sda, PinName scl, int frequency_hz): 
+NodeFlow::NodeFlow(PinName write_control, PinName sda, PinName scl, int frequency_hz): 
                          _dm(write_control, sda, scl, frequency_hz){
 
  }
@@ -31,88 +11,8 @@ NodeFlow::~NodeFlow() {
 
 }
 
-   
-  void NodeFlow::start()
-{   
-    // wdg.kick(); 
-    // _init_rtc();
-    
-    }
 
-/**
- *
- */   
- int NodeFlow::initialise()
-{   
-
- int status = -1;
- status=_dm.init_filesystem();
- 
-   //wdg.kick(); 
-   // _init_rtc();
-    
-    }
-
-/**
- * This should only be use on first wakeup or reset device as it will ovewrite time and sensor Ids
- */
-// bool NodeFlow::registerSensor(const uint8_t sensorId=NULL,const uint8_t seadingTime=NULL)
-// {
-//     //Create an array of sensor ids with sensortypes in eeprom, with their reading time
-
-// }
-
-/** 
-    if its the first time (in case of reset set the value to the smallest directly)
-    for each sensor calculate next reading time get the smallest 
-    and set it as the next reading timer
- */ 
-
-int NodeFlow::set_reading_time(int arr[],int s){
-
-//int array[]={0,0};   //each time the user creates a new sensor  
-    
-    // int time_comparator=0; 
-    // int temp=0;
-
-    
-    // int sizeofarray=sizeof(arr)/sizeof(arr[0]);
-    // for (int i=0; i<=sizeofarray; i++){
-    //     //int temp=arr[i];
-    //     if (temp>=arr[i] && arr[i]!=0){
-    //         temp=arr[i];   
-    //     }
-    // time_comparator=temp;
-    //printf("\r\nMin: %d , %d",time_comparator, sizeofarray);
-    //printf("\r\n %d",time_comparator);
-    // } 
-    // return time_comparator;
-}
-/**
- * 
- */
-int NodeFlow::SensorsHandler(){
-
-    
-
-
-
-}
-void NodeFlow::reset(){
-
-}
-
-
-// void ModemSend(int time)
-// {
-    
-// }
-
-/**
- *
- */
-
-   union DeviceConfig
+union DeviceConfig
 {
     struct 
     {
@@ -156,100 +56,10 @@ enum Filenames
     SensorB_n      = 2
 };
 
+int NodeFlow::initialise(){
+ int status = -1;
+ status=_dm.init_filesystem();
+ 
 
- //RTC_HandleTypeDef RtcHandle;
-
-// void NodeFlow::_init_rtc() {
-//    PlatformMutex *mtx = new PlatformMutex;
-//    mtx->lock();
-//    rtc_init();
-//    mtx->unlock();
-//    delete(mtx);
-// }
-// void NodeFlow::SystemPower_Config() {
-//    HAL_Init();
-//    GPIO_InitTypeDef GPIO_InitStructure;
-//    __HAL_RCC_PWR_CLK_ENABLE();
-//    HAL_PWREx_EnableUltraLowPower();
-//    HAL_PWREx_EnableFastWakeUp();
-//    __HAL_RCC_WAKEUPSTOP_CLK_CONFIG(RCC_STOP_WAKEUPCLOCK_MSI);
-//    __HAL_RCC_GPIOA_CLK_ENABLE();
-//    __HAL_RCC_GPIOB_CLK_ENABLE();
-//    __HAL_RCC_GPIOC_CLK_ENABLE();
-//    __HAL_RCC_GPIOD_CLK_ENABLE();
-//    __HAL_RCC_GPIOH_CLK_ENABLE();
-//    __HAL_RCC_GPIOE_CLK_ENABLE();
-//    GPIO_InitStructure.Pin = GPIO_PIN_All;
-//    GPIO_InitStructure.Mode = GPIO_MODE_ANALOG;
-//    GPIO_InitStructure.Pull = GPIO_NOPULL;
-//    HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
-//    HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
-//    HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
-//    HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
-//    HAL_GPIO_Init(GPIOH, &GPIO_InitStructure);
-//    HAL_GPIO_Init(GPIOE, &GPIO_InitStructure);
-//    __HAL_RCC_GPIOA_CLK_DISABLE();
-//    __HAL_RCC_GPIOB_CLK_DISABLE();
-//    __HAL_RCC_GPIOC_CLK_DISABLE();
-//    __HAL_RCC_GPIOD_CLK_DISABLE();
-//    __HAL_RCC_GPIOH_CLK_DISABLE();
-//    __HAL_RCC_GPIOE_CLK_DISABLE();
-// }
-
-
-//  void NodeFlow::rtc_set_wake_up_timer_s(uint32_t delta) {
-//    uint32_t clock = RTC_WAKEUPCLOCK_CK_SPRE_16BITS;
-//    // HAL_RTCEx_SetWakeUpTimer_IT will assert that delta is 0xFFFF at max
-//    if (delta > 0xFFFF) {
-//        delta -= 0x10000;
-//        clock = RTC_WAKEUPCLOCK_CK_SPRE_17BITS;
-//    }
-//    RtcHandle.Instance = RTC;
-//    HAL_StatusTypeDef status = HAL_RTCEx_SetWakeUpTimer_IT(&RtcHandle, delta, clock);
-//    if (status != HAL_OK) {
-//        NVIC_SystemReset();
-//     }
-// }
-// void NodeFlow::clear_uc_wakeup_flags() {
-//    __HAL_RCC_CLEAR_RESET_FLAGS();
-//    SET_BIT(PWR->CR, PWR_CR_CWUF);
-// }
-// static WakeupType get_wakeup_type() {
-//    if(__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST)) {
-//        return WAKEUP_RESET;
-//    }
-//    if(READ_BIT(RTC->ISR, RTC_ISR_WUTF)) {
-//        return WAKEUP_TIMER;
-//    }
-//    if(READ_BIT(PWR->CSR, PWR_CSR_WUF)) {
-//        return WAKEUP_PIN;
-//    }
-//    return WAKEUP_RESET;
-// }
-
-// void NodeFlow::standby(int seconds, bool wkup_one, bool wkup_two) {
-//    SystemPower_Config();
-//    core_util_critical_section_enter();
-//    clear_uc_wakeup_flags();
-//    // Enable wakeup timer.
-//    rtc_set_wake_up_timer_s(seconds);
-//    if(wkup_one) {
-//        HAL_PWR_EnableWakeUpPin(PWR_WAKEUP_PIN1);
-//    }
-//    else {
-//        HAL_PWR_DisableWakeUpPin(PWR_WAKEUP_PIN1);
-//    }
-//    if(wkup_two) {
-//        HAL_PWR_EnableWakeUpPin(PWR_WAKEUP_PIN2);
-//    }
-//    else {
-//        HAL_PWR_DisableWakeUpPin(PWR_WAKEUP_PIN2);
-//    }
-//    HAL_PWR_EnterSTANDBYMode();
-//    // this should not happen...
-//    //rtc_deactivate_wake_up_timer();
-//    core_util_critical_section_exit();
-//    // something went wrong, let's reset
-//    NVIC_SystemReset();
-// }
-
+    
+}
--- node_flow.h
+++ node_flow.h
@@ -1,170 +1,18 @@
-
-/**
-  * @file    NodeFlow.h
-  * @version 1.0.0
-  * @author  
-  * @brief   Header file of the SX1276 driver module. 
-  * A library which implements a software flow for thingpilot Node Core powered modules. 
-  * Controls the device configuration, OTA config updates, security, sensor read timing, 
-  * telemetry transmit timing and provides pure virtual functions for the application developer 
-  * to implement application device specific logic
-  */
-
-#ifndef MBED_NODEFLOW_H
-#define MBED_NODEFLOW_H
-
-/** Includes 
- */
-//#include "TPL5010.h"
-//#include "tformatter.h"
 #include "DataManager.h"
-//#include "SaraN2.h"
-//#include "LorawanTP.h"
-//#include "rtc_api_hal.h"
 #include "board.h"
 
-
-/** Base class for the Nodeflow
- */      
 class NodeFlow {
 
-    public:
+    public: 
+    NodeFlow(PinName write_control, PinName sda, PinName scl, int frequency_hz);
 
-    /**/ 
-    // enum WakeupType {
-    // WAKEUP_RESET,
-    // WAKEUP_TIMER,
-    // WAKEUP_PIN
-    // };
-   /** Enum
-    */            
-    
-    // enum {
-    //     NODE_FLOW_OK=0
-    // };
-   /**
-    * Type of sensors. This will be used for 
-    */
-    // typedef enum {
-    //     TEMP_C, HMDTY_RH, LIGHT_LX, PRESS_PA, AUX_2, AUX_3, AUX_4
-    // } sensor;
-        
-    /**
-     * Constructor
-     */
-     NodeFlow(PinName write_control, PinName sda, PinName scl, int frequency_hz);
-    
-    /**
-     * Destructor
-     */
-     ~NodeFlow();
-    
-    /**
-     * Generic Initialise device, init_eeprom, set flag to debug mode
-     */
-     int initialise(); 
-     /**
-     * Start
-     */
-     void start(); 
-    
+    ~NodeFlow();
 
-    /**
-     * Return WakeupType. 
-     */
-     //static WakeupType get_wakeup_type();
-
-     
-
-     /**
-     * Reset. Reset the eeprom to default 
-     */
-     void reset();
-
-    /**
-     *  Configure Sleep
-     */
-     /**
-     *  Sets the device in standby mode. Handles the times, 
-     *  @param periodic_intervals   Sets periodic time intervals for device to wakeup
-     *  periodic times must equal with the highest common factor
-     *  @param interrupt_sleep_time Sets time that the interrupt will sleep after first wakeup
-     *  ex. enter_standby(periodic_intervals, interrupt_sleep_time);
-     */
-
-     void enter_standby(int intervals=NULL);
-     //void standby(int seconds, bool wkup_one, bool wkup_two);
-    int8_t _sleep	(const uint32_t 	sleepingM, const uint8_t interrupt1 =NULL);	
-    /**
-     * Use this in case of constant periodic sensing from all sensors
-     * 
-     */
-     void setReportIntervalMinutes(int periodic_intervals=NULL);
-    /**
-     * Defines how long the device will ignore the interrupt after an interrupt goes off
-     *
-     */
-     #if defined (MBED_INTERRUPTIN_H)  //if interrupt is defined 
-     void set_max_interrupt_sleep(int interrupt_sleep_time=NULL);
-     #endif
-    
-    
-    /** This should only be use on first wakeup or reset device as it will ovewrite time and sensor Ids 
-     *  The sensors should present themselves before they start reporting sensor data to the controller. 
-     *  @param SensorId - The unique id you want to choose for the sensor connected to the thing pilot. (ex Range 0-254)
-     *  @param sensorType - The sensor type you want to create.
-     *
-     *  Returns true if sensor is initialised
-     */
-    virtual bool registerSensor(const uint8_t sensorId=NULL, const uint8_t readingTime=NULL);
-    
-    int set_reading_time(int arr[],int n);
-    /**
-     * Calculates the next sensor reading timer
-     * 
-     */
-     //int next_reading_time();
-
-    /**
-     * Wait without setting the device to standby
-     */
-     void wait (const uint32_t waitingMS);
-
-    /** Check wich Sensors are set to be read
-     *  Return which sensors to be read, return next reading time for each
-     */
-     int  SensorsHandler();
-
-     void TimeTriggerHandler();
-
-
-    void   SystemPower_Config();
-    static void rtc_set_wake_up_timer_s(uint32_t delta);
-    void clear_uc_wakeup_flags();
-    /**/
+    int initialise(); 
 
     
-    int ModemSend(uint8_t port,const uint8_t *data, uint8_t length,int periodic_intervals,int flags);
+    DataManager _dm;
 
-
-    private:
-
-    static RTC_HandleTypeDef RtcHandle;
-    
-    void _init_rtc();
 
     
-    /* Actions */
-   // void init_eeprom(); from DataManager
-    // void get_periodic_sensors(); //Sensors driver
-    // void get_trigger_sensors(); //Sensors driver
-    
-     float get_supply_voltage(); //treat as a sensor 
-
-     //TPL5010 _wdg;
-     DataManager _dm;
-
 };
-
-
-#endif
